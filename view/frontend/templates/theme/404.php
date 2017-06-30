0125fe3e857d1f002adf9ccfd9cd1b3171d60b23
Vazgen Ovakimyan <vdevx86job@gmail.com>
2017-06-30 15:57:16 +0300

    WordPress theme: 404 template added

---
+++ view/frontend/templates/theme/404.php
@@ -0,0 +1,10 @@
+<?php checkCompatibility(); global $theme; ?>
+<section id="blog" <?php $theme->bodyClass(); ?>>
+    <article>
+        <div class="post-content">
+            <div class="message error empty">
+                <div><?php $theme->_e('It looks like nothing was found at this location. Maybe try a search?'); ?></div>
+            </div>
+        </div>
+    </article>
+</section>
