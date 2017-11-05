767ca22393fb3f203f84c1f128fe000729758bf5
Vazgen Ovakimyan <vdevx86job@gmail.com>
2017-11-05 03:37:35 +0200

    CR-0009

---
+++ Block/System/Config/Form/Field/Install.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright © 2017 Walk with Magento
+ * See COPYING.txt for license details
+ */
+
+/**
+ * Free integration between Magento 2 and WordPress
+ *
+ * @author Ovakimyan Vazgen <vdevx86job@gmail.com>
+ * @copyright 2017 Walk with Magento (http://wwm-integrations.in.ua)
+ * @license https://opensource.org/licenses/OSL-3.0 Open Software License ("OSL") v. 3.0
+ * @copyright 2017 Ovakimyan Vazgen <vdevx86job@gmail.com>
+ */
+
+namespace Wwm\Blog\Block\System\Config\Form\Field;
+
+class Install extends \Magento\Config\Block\System\Config\Form\Field
+{
+    
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+        return parent::render($element);
+    }
+    
+}
